f4ede792ebba13cefab4b861e8bbbb881851e56d
Diego Dindurra <diego@tipit.net>
2018-05-30 16:37:00 -0300

    Adjustments on sites configuration.

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -6,58 +6,37 @@ DOMAINS=`get_hosts "${DOMAIN}"`
 SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
 WP_VERSION=`get_config_value 'wp_version' 'latest'`
 WP_TYPE=`get_config_value 'wp_type' "single"`
-DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
-DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
 
-# Make a database, if we don't already have one
 echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
 mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
 mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
-echo -e "\n DB operations done.\n\n"
+mysql -u root --password=root ${DB_NAME} < /srv/database/backups/${DB_NAME}.sql
 
-# Nginx Logs
+echo -e "\nReplacing production data into db tables, if set."
+cat ${VVV_CONFIG} | shyaml get-values-0 sites.${SITE_ESCAPED}.replace_strings  2> /dev/null|
+    while IFS='' read -r -d '' key &&
+          IFS='' read -r -d '' value; do
+              echo "'$key' -> '$value'"
+              noroot wp search-replace ${key} ${value} --skip-columns=guid --skip-packages --skip-themes --skip-plugins --path=${VVV_PATH_TO_SITE}
+    done
+
+echo -e "\nCopying media, if set."
+cat ${VVV_CONFIG} | shyaml get-values-0 sites.${SITE_ESCAPED}.media_folders  2> /dev/null|
+    while IFS='' read -r -d '' key &&
+          IFS='' read -r -d '' value; do
+              echo "'$key' -> '${VVV_PATH_TO_SITE}/$value'"
+              [ -d "${VVV_PATH_TO_SITE}/$value" ] || noroot mkdir "${VVV_PATH_TO_SITE}/$value"
+		      noroot cp -r "$key/."  "${VVV_PATH_TO_SITE}/$value"
+    done
+	
+echo -e "\nSetting NGINX logs."
 mkdir -p ${VVV_PATH_TO_SITE}/log
 touch ${VVV_PATH_TO_SITE}/log/error.log
 touch ${VVV_PATH_TO_SITE}/log/access.log
 
-# Install and configure the latest stable version of WordPress
-if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-load.php" ]]; then
-    echo "Downloading WordPress..."
-	noroot wp core download --version="${WP_VERSION}"
-fi
-
-if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-config.php" ]]; then
-  echo "Configuring WordPress Stable..."
-  noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-define( 'WP_DEBUG', true );
-PHP
-fi
-
-if ! $(noroot wp core is-installed); then
-  echo "Installing WordPress Stable..."
-
-  if [ "${WP_TYPE}" = "subdomain" ]; then
-    INSTALL_COMMAND="multisite-install --subdomains"
-  elif [ "${WP_TYPE}" = "subdirectory" ]; then
-    INSTALL_COMMAND="multisite-install"
-  else
-    INSTALL_COMMAND="install"
-  fi
-
-  noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
-else
-  echo "Updating WordPress Stable..."
-  cd ${VVV_PATH_TO_SITE}/public_html
-  noroot wp core update --version="${WP_VERSION}"
-fi
-
+echo -e "\nSetting NGINX custom site file."
 cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 
-if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
-    sed -i "s#{{TLS_CERT}}#ssl_certificate /vagrant/certificates/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-    sed -i "s#{{TLS_KEY}}#ssl_certificate_key /vagrant/certificates/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-else
-    sed -i "s#{{TLS_CERT}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-    sed -i "s#{{TLS_KEY}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-fi
+echo -e "\nSetting hosts file."
+sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-hosts"
